da2627e773ad429c958826345c26a067396f44a7
Tangerine7788 <93781891+Tangerine7788@users.noreply.github.com>
2021-11-20 20:58:03 +0600

    Hw5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,15 @@ package com.company;
 
 public class Main {
 
+
     public static void main(String[] args) {
+
+        for (int i = 0; i < createHeroes().length; i++) {
+            System.out.println(createHeroes()[i].getDamage());
+            System.out.println(createHeroes()[i].getHealth());
+            System.out.println(createHeroes()[i].getSuperpower());
+        }
+
 	Boss boss = new Boss();
     boss.setHealth(600);
         System.out.println(boss.getHealth());
@@ -10,5 +18,14 @@ public class Main {
         System.out.println(boss.getDamage());
         boss.setProtectType("Magical!");
         System.out.println(boss.getProtectType());
+    }
+    public static Hero[] createHeroes() {
+        Hero WonderWoman = new Hero(700,70,"super strength");
+        Hero SuperGirl = new Hero(500,70,"super speed");
+        Hero SuperWoman = new Hero(600,70,"super jump");
+
+        Hero[] heroes ={WonderWoman,SuperWoman,SuperGirl};
+
+        return heroes;
     }
 }
